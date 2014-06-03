b2ca084c3989365e9ea67a3dd40896eaea43bd8d
Alexandre Prates <ajfprates@gmail.com>
2014-06-03 16:08:25 -0300

    Coding buildable and generator (not completed)

--- lib/buildable.rb
+++ lib/buildable.rb
@@ -1,5 +1,33 @@
-require "buildable/version"
+require "./lib/buildable/version"
+require "./lib/buildable/generator"
 
 module Buildable
-  # Your code goes here...
+  class << self
+
+    def run(name)
+      create_directories
+      create_files(name)
+    end
+
+    private
+
+    def create_directories
+      p from_here './build'
+      # File.mkdir(from_here './build')
+      # File.mkdir(from_here './build/debian')
+      # File.mkdir(from_here './etc')
+      # File.mkdir(from_here './etc/init.d')
+    end
+
+    def create_files(name)
+      # templates =
+      # generator = Buildable::Generator.new(name)
+      # generator
+    end
+
+    def from_here(dir)
+      File.expand_path(dir)
+    end
+
+  end
 end
---
+++ lib/buildable/generator.rb
@@ -0,0 +1,18 @@
+require 'erb'
+
+module Buildable
+  class Generator
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def make(template, destination)
+      @template = File.read(template)
+      content = ERB.new(@template).result binding
+      File.open(destination, 'w') { |f| f.write content }
+    end
+
+  end
+end
