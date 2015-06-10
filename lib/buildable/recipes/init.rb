c0fb72a70da2dfe3e3cc617dc7aed3fd27b0b30b
Alexandre Prates <ajfprates@gmail.com>
2015-06-10 17:51:22 -0300

    fixing .env creation

--- lib/buildable/recipes/init.rb
+++ lib/buildable/recipes/init.rb
@@ -1,9 +1,12 @@
 module Buildable::Recipe
   recipe :init do
-    puts "Creating Procfile"
-    Buildable::FileMaker.template 'Procfile'
     puts "Creating .buildable.yml"
     Buildable::FileMaker.template '.buildable.yml'
-    puts "Please edit .buildable.yml to setup application"
+    puts "Creating .env"
+    Buildable::FileMaker.template '.env'
+    puts "Creating Procfile"
+    Buildable::FileMaker.template 'Procfile'
+
+    puts "Please edit all files above to setup your application"
   end
 end
