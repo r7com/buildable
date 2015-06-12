738b171bba51c02b2e805882d9a9e1d5fd277cc8
Alexandre Prates <ajfprates@gmail.com>
2015-06-12 14:22:20 -0300

    Fixing missing PTY return code

--- lib/buildable/shell.rb
+++ lib/buildable/shell.rb
@@ -8,8 +8,9 @@ module Buildable::Shell
   end
 
   def do(command)
-    PTY.spawn(command) do |r,w,pid|
+    PTY.spawn("#{command} 2>&1") do |r,w,pid|
       r.each { |line| puts "\t#{line}" } rescue nil # prevents error when process ending
+      Process.wait(pid)
     end
   rescue PTY::ChildExited
     true
@@ -18,4 +19,5 @@ module Buildable::Shell
   def do_quiet(command)
     %x{#{command} 2>&1}.chomp
   end
+
 end
