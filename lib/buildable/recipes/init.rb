40351a59cd9fb3c5c1bb0876ed68c383f238339c
Alexandre Prates <ajfprates@gmail.com>
2016-02-19 16:53:24 -0200

    fixing init scripts

--- lib/buildable/recipes/init.rb
+++ lib/buildable/recipes/init.rb
@@ -2,8 +2,8 @@ module Buildable::Recipe
   recipe :init do
     puts "Creating .buildable.yml"
     Buildable::FileMaker.template '.buildable.yml'
-    puts "Creating .env"
-    Buildable::FileMaker.template '.env'
+    puts "Creating production.env"
+    Buildable::FileMaker.template 'production.env'
     puts "Creating Procfile"
     Buildable::FileMaker.template 'Procfile'
 
