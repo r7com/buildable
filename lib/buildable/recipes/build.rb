ec3f348b36a797ba4d0b207f02b3e06455014978
Alexandre Prates <ajfprates@gmail.com>
2016-03-14 14:56:54 -0300

    Adding support to pre/post install scripts

--- lib/buildable/recipes/build.rb
+++ lib/buildable/recipes/build.rb
@@ -37,7 +37,20 @@ module Buildable::Recipe
     puts "* Creating package"
     version = Buildable::Shell.do_quiet %Q{git describe --abbrev=0 --match="[0-9]*\.[0-9]*\.[0-9]*"}
     raise "Can't define build version, please check git describe" unless Buildable::Shell.success?
-    result = Buildable::Shell.do_quiet "bundle exec fpm -s dir -t deb --name r7-#{Buildable.config.project_name.downcase} --version #{version} --architecture all --maintainer R7 --force --package ./pkg --prefix / --description #{Buildable.config.description.inspect} -C '#{Buildable::BUILD_ROOT_DIR}' ./etc ./r7"
+    # result = Buildable::Shell.do_quiet "bundle exec fpm -s dir -t deb --name r7-#{Buildable.config.project_name.downcase} --version #{version} --architecture all --maintainer R7 --force --package ./pkg --prefix / --description #{Buildable.config.description.inspect} #{Buildable.add_pre_install} #{Buildable.add_post_install} -C '#{Buildable::BUILD_ROOT_DIR}' ./etc ./r7"
+    params = {'-s' => 'dir',
+              '-t' => 'deb',
+              '--name' => 'r7-#{Buildable.config.project_name.downcase}',
+              '--version' => version,
+              '--architecture' => 'all',
+              '--package' => './pkg',
+              '--prefix' => '/',
+              '--description' => Buildable.config.description,
+              '--force' => nil,
+              '--pre-install' => Buildable.config.pre_install if File.exist? Buildable.config.pre_install,
+              '--post-install' => Buildable.config.post_install if File.exist? Buildable.config.post_install,
+              '-C' => "{Buildable::BUILD_ROOT_DIR} ./etc ./#{Buildable.build_app_dir}"}
+    result = Buildable::Shell.do_quiet 'bundle exec fpm', params
     raise "Can't create package, error:\n#{result}" unless Buildable::Shell.success?
     package_name = result.match(/:path=>"\.\/pkg\/([^"]*)/)[1]
     puts "\tPackage created #{package_name}"
