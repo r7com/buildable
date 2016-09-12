4bf3674fe96b966c4dfa38e9fce26b89927bc901
Alexandre Prates <ajfprates@gmail.com>
2016-09-12 10:08:14 -0300

    Refactoring rake tasks modules

--- lib/buildable/rake_task.rb
+++ lib/buildable/rake_task.rb
@@ -1,3 +1,30 @@
+require 'rake'
 require 'buildable'
 
-Buildable::RakeHelper.install_tasks
+module Buildable::RakeTask
+  include Rake::DSL if defined? Rake::DSL
+  extend self
+
+  def load
+    namespace :buildable do
+      set_init_task
+      set_build_task
+    end
+  end
+
+  private
+
+  def set_init_task
+    desc "Create package buildable structure"
+    task :init do
+      Buildable.init
+    end
+  end
+
+  def set_build_task
+    desc "Make debian (deb) package"
+    task :build do
+      Buildable.build
+    end
+  end
+end
